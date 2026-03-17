53b315b195c2ee8ff0daf16711ed4ef2fdf8b07b
Balraj B <balraj2394@gmail.com>
2026-03-17 15:54:36 +0530

    Update SubArrayMax.java

--- Day-2/SubArrayMax.java
+++ Day-2/SubArrayMax.java
@@ -1,5 +1,6 @@
+
 import java.util.Scanner;
-public class SubArrayMax{
+public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
@@ -7,16 +8,17 @@ public class SubArrayMax{
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        int sum=0 ,max=0;
-        for(int i=0;i<n;i++){
-            sum=0;
-            for(int j=i;j<n;j++){
-                sum+=arr[j];
-                if(sum>max){
-                    max=sum;
-                }
-            }
+        int sum=arr[0] ,maxSum=arr[0];
+        for(int i=1;i<n;i++){
+           if(sum>=0){
+               sum=sum+arr[i];
+           }else{
+               sum=arr[i];
+           }
+           if(sum>maxSum){
+               maxSum=sum;
+           }
         }
-        System.out.println(max);
+        System.out.println(maxSum);
     }
 }
